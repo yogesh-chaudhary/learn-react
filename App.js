15d951235cb983ff804818787fa7b6f51e3a3319
Yogesh Chaudhary <yogeshchaudhary@Yogeshs-MacBook-Pro.local>
2024-11-12 18:43:37 +0530

    Parcel Configuration Added.

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am an h1 tag."),
